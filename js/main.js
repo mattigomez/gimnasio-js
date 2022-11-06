1b949974d4aab6fc63a7f04df49ed2d4947b87dd
MatiasGomez <gomezmati@hotmail.com>
2022-11-06 15:56:51 -0300

    segunda entrega

--- js/main.js
+++ js/main.js
@@ -1,141 +1,3 @@
-//funciones
-//declaracion
-
-/* function saludar() {
-  console.log("HOLA TAROLA");
-} */
-//llamado o invocacion
-/* saludar();
-saludar();
-saludar(); */
-
-/* let nombre = prompt("Ingresa tu nombre");
-console.log("El nombre ingresado es : " + nombre);
-nombre = prompt("Ingresa tu nombre");
-console.log("El nombre ingresado es : " + nombre);
-nombre = prompt("Ingresa tu nombre");
-console.log("El nombre ingresado es : " + nombre); */
-/* let nombre;
-
-function ingresoNombre() {
-  nombre = prompt("Ingresa tu nombre");
-  // console.log("El nombre ingresado es : " + nombre);
-}
- */
-/* function mostrar() {
-  console.log(nombre);
-} */
-//ingresoNombre();
-//mostrar();
-//con parametros
-/* function conParametros(parametro1, parametro2) {
-  console.log(parametro1);
-  console.log(parametro2);
-}
-let nombre2 = prompt("ingresa el nombre");
-conParametros("Hola", nombre2); */
-
-//let resultado = 0;
-
-/* function sumar(primerNumero, segundoNumero) {
-  resultado = primerNumero + segundoNumero;
-} */
-/* 
-function mostrar(mensaje, valor) {
-  console.log(mensaje + valor);
-} */
-
-//mostrar(resultado);
-
-//sumar(33, 99);
-//mostrar(resultado);
-//sumar(32, 9);
-
-//mostrar(resultado);
-
-//return
-/* function sumar(primerNumero, segundoNumero) {
-  return primerNumero + segundoNumero;
-}
-function restar(primerNumero, segundoNumero) {
-  return primerNumero - segundoNumero;
-}
-
-function dividir(primerNumero, segundoNumero) {
-  if (isNaN(primerNumero) || isNaN(segundoNumero)) {
-    return "los valores no son numricos";
-  }
-
-  if (segundoNumero == 0) {
-    return "Error";
-  } else {
-    return primerNumero / segundoNumero;
-  }
-} */
-
-/* let resultadoSuma = sumar(45, 15);
-let resultadoResta = restar(12, 9);
-let resDiv = dividir(12, 0);
-let resDiv2 = dividir(12, 3); */
-/* mostrar("El resultado de la suma es: ", resultadoSuma);
-mostrar("El resultado de la resta es: ", resultadoResta); */
-/* 
-console.log(resDiv);
-console.log(resDiv2);
-
-console.log(sumar(resultadoSuma, resDiv2)); */
-//SCOPE
-//variable global
-/* 
-let variable = "Soy una variable global";
- */
-//console.log(variable);
-
-/* function cambiar() {
-  variable = "Soy la variable global modificada desde una funcion";
-  console.log(variable);
-} */
-
-//cambiar();
-
-/* if (true) {
-  variable = "Soy la variable global modificada desde un if";
-  //console.log(variable);
-}
-
-let variableLocal = "Soy una variable que estoy fuera de la funcion";
- */
-//Variable
-/* function cambiarVariableLocal() {
-  let variableLocal = "Soy una variable local dentro de una estructura";
-  console.log(variableLocal);
-} */
-
-//cambiarVariableLocal();
-
-//console.log(variableLocal);
-
-//Funcion anonima
-
-/* const funcionAnonima = function () {
-  console.log("Soy una funcion anonima");
-};
-funcionAnonima();
-
-const suma = function (n1, n2) {
-  return n1 + n2;
-};
-let numero1 = prompt("Ingresa el primer numero");
-let numero2 = prompt("Ingresa el segundo numero");
-console.log(suma(numero1, numero2));
-
-//function flecha
-
-const resta = (n1, n2) => n1 - n2; //ES6 return es implicito
-
-console.log(resta(21, 4));
- */
-
 let DNI="1234"
 
 function login (){
@@ -144,7 +6,7 @@ function login (){
   for (let i = 3;i>=0; i--){
     let userDNI= prompt("Ingresa tu DNI");
     if (userDNI === DNI){
-      alert("Bienvenido a Rosario Gym");
+      alert("Bienvenido a Abasto Gym");
       ingresar= true;
       break;
     }
@@ -197,3 +59,45 @@ else{
 }
 
 alert("Gracias por confiar en nosotros. A entrenar!")
+
+const sucursales= [
+  "Rosario",
+  "Buenos Aires",
+  "Cordoba",
+  "Mendoza",
+]
+
+const select = document.querySelector('select[name="sucursales"]')
+console.log(select)
+
+sucursales.forEach(sucursal=>{
+  let option = document.createElement("option");
+
+  option.value=sucursal;
+  option.innerText=sucursal;
+
+  select.appendChild(option)
+})
+
+select.addEventListener("change",()=>{
+  let option= select.options[select.selectedIndex].value;
+  console.log(option);
+})
+
+// funcion para filtrar productos por precio dado
+
+const productos = [
+  { id: 1, nombre: "proteina", precio: 4500 },
+  { id: 2, nombre: "creatina", precio: 4000 },
+  { id: 3, nombre: "colageno", precio: 2500 },
+  { id: 4, nombre: "pasta de mani 1kg", precio: 3000 },
+  { id: 5, nombre: "barra proteica", precio: 200 },
+];
+
+function filtrarPorPrecio(arr,filtro){
+  return arr.filter((producto)=>{
+    return producto.precio <= filtro;
+  })
+}
+const menorPrecio= filtrarPorPrecio(productos,3500)
+console.log(menorPrecio)
